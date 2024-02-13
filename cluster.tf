eab7f40bed2f256bc2195d37b6f69ad9e3e26b55
vinod sjml <vinsjml@gmail.com>
2024-02-13 16:28:26 +0530

    push

--- cluster.tf
+++ cluster.tf
@@ -5,7 +5,7 @@ resource "aws_docdb_cluster" "docdb" {
   master_password         = "roboshop1"
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_db_subnet_group.docdb_subnet_group.name
-  vpc_security_group_ids  = [aws_security_group.docdb.id]
+  vpc_security_group_ids  = [aws_security_group.allows_docdb.id]
   port = 27017
 }
 
